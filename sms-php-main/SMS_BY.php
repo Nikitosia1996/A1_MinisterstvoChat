026b63e67903745d9900c1c8c63c09953fd8d8c4
Hancharou.Uladzislau <hancharou@rncpmt.by>
2024-02-09 10:38:34 +0300

    sms

---
+++ sms-php-main/SMS_BY.php
@@ -0,0 +1,374 @@
+<?php
+  class SMS_BY
+  {
+    private $token;
+    private $API_URL    = "https://app.sms.by/api/v1/";
+    private $API_URL_v2 = "https://app.sms.by/api/v2/";
+
+    /**
+     * $token - API KEY 
+     */
+    public function __construct($token) {
+       if (!empty($token))
+          $this->token = $token;
+      else {
+          exit("Код токена не указан. Вы можете получить его здесь: https://app.sms.by/user-api/token");
+      }
+    }
+
+    /**
+     * Отправляет команду на API_URL.
+     * Если команда обработана успешно, возвращает ответ от API в виде объекта.
+     * Если команда обработана неуспешно - передаёт ошибку методу error() и возвращает false.
+     * $command - команда API
+     * $params - ассоциативный массив, ключи которого являются названиями параметров команды кроме token, значения - их значениями.
+     * token в $params передавать не нужно.
+     * Необязательный параметр, так как для таких команд, как getLimit, getMessagesList, getPasswordObjects никаких параметров передавать не нужно.
+     */
+    private function sendRequest($command, $params = array(), $method = 'get') {
+        if ($method=='get') {
+            $url = $this->API_URL . $command . '?token=' . $this->token;
+            if (!empty($params)) {
+                foreach ($params as $k => $v)
+                    $url .= '&' . $k . '=' . urlencode($v);
+            }
+        }
+        else {
+            $url = $this->API_URL . $command;
+        }
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+        if ($method=='post') {
+          $params['token'] = $this->token;
+          curl_setopt($ch, CURLOPT_POST, 1);
+          curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+        }
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $result = json_decode($result);
+        if (isset($result->error)) {
+            $this->error($result->error);
+            return false;
+        }
+        else
+            return $result;
+    }
+
+    /**
+     * Error handling 
+     * Обрабатывает ошибки.
+     * Здесь может быть любой код, обрабатывающий пришедшую по API ошибку, соответствующий вашему приложению.
+     * $error - текст ошибки
+     */
+    private function error($error) {
+        trigger_error("<b>SMS.BY error:</b> $error");
+    }
+
+    /**
+     * Get balance
+     * Метод-обёртка для команды getBalance
+     */
+    public function getBalance() {
+        return $this->sendRequest("getBalance");
+    }
+
+    /**
+     * Метод-обёртка для команды getLimit
+     */
+    public function getLimit() {
+        return $this->sendRequest('getLimit');
+    }
+
+    /**
+     * Метод-обёртка для команды createSMSMessage
+     * $message - текст создаваемого сообщения
+     * $alphaname_id - ID альфа-имени, необязательный параметр
+     */
+    public function createSMSMessage($message, $alphaname_id = 0) {
+        $params['message'] = $message;
+        if (!empty($alphaname_id))
+            $params['alphaname_id'] = (integer)$alphaname_id;
+        return $this->sendRequest('createSmsMessage', $params);
+    }
+
+    /**
+     * Метод-обёртка для команды checkSMSMessageStatus
+     * $message_id - ID созданного сообщения
+     */
+    public function checkSMSMessageStatus($message_id) {
+        $params['message_id'] = (integer)$message_id;
+        return $this->sendRequest('checkSMSMessageStatus', $params);
+    }
+
+    /**
+     * Метод-обёртка для команды getMessagesList
+     */
+    public function getMessagesList() {
+        return $this->sendRequest('getMessagesList');
+    }
+
+    /**
+     * Метод-обёртка для команды sendSms
+     * $message_id - ID созданного сообщения
+     * $phone - номер телефона в формате 375291234567
+     */
+    public function sendSms($message_id, $phone) {
+        $params['message_id'] = (integer)$message_id;
+        $params['phone'] = $phone;
+        return $this->sendRequest('sendSms', $params);
+    }
+
+    /**
+     * The fastest was to send single sms. Use this method if you send 1 sms to 1 user. If you send 1 message to Many users, please use 
+     *   createSMSMessage and sendSms
+     * Метод-обёртка для команды sendQuickSms - отправка смс-сообщения без предварительного его создания
+     * $message - текст созданного сообщения
+     * $phone - номер телефона в формате 375291234567
+     */
+    public function sendQuickSms($message, $phone) {
+      if(!empty($message) && !empty($phone))
+      {
+        $params['message'] = $message;
+        $params['phone'] = $phone;
+        return $this->sendRequest('sendQuickSms', $params);
+      }
+      else
+      {
+         return null;
+      }
+    }
+
+    /**
+     * Метод-обёртка для команды checkSMS
+     * $sms_id - ID отправленного SMS
+     */
+    public function checkSMS($sms_id) {
+        $params['sms_id'] = (integer)$sms_id;
+        return $this->sendRequest('checkSMS', $params);
+    }
+
+    /**
+     * Метод-обёртка для команды createPasswordObject
+     * $type_id - тип создаваемого объекта пароля, может принимать значения letters, numbers и both
+     * $len - длина создаваемого объекта пароля, целое число от 1 до 16
+     */
+    public function createPasswordObject($type_id, $len) {
+        $params['type_id'] = $type_id;
+        $params['len'] = (integer)$len;
+        return $this->sendRequest('createPasswordObject', $params);
+    }
+
+    /**
+     * Метод-обёртка для команды editPasswordObject
+     * $password_object_id - ID созданного объекта пароля
+     * $type_id - тип создаваемого объекта пароля, может принимать значения letters, numbers и both
+     * $len - длина создаваемого объекта пароля, целое число от 1 до 16
+     */
+    public function editPasswordObject($password_object_id, $type_id, $len) {
+        $params['id'] = (integer)$password_object_id;
+        $params['type_id'] = $type_id;
+        $params['len'] = (integer)$len;
+        return $this->sendRequest('editPasswordObject', $params);
+    }
+
+    /**
+     * Метод-обёртка для команды deletePasswordObject
+     * $password_object_id - ID созданного объекта пароля
+     */
+    public function deletePasswordObject($password_object_id) {
+        $params['id'] = (integer)$password_object_id;
+        return $this->sendRequest('deletePasswordObject', $params);
+    }
+
+    /**
+     * Метод-обёртка для команды getPasswordObjects
+     */
+    public function getPasswordObjects() {
+        return $this->sendRequest('getPasswordObjects');
+    }
+
+    /**
+     * Метод-обёртка для команды getPasswordObject
+     * $password_object_id - ID созданного объекта пароля
+     */
+    public function getPasswordObject($password_object_id) {
+        $params['id'] = (integer)$password_object_id;
+        return $this->sendRequest('getPasswordObject', $params);
+    }
+
+    /**
+     * Метод-обёртка для команды sendSmsMessageWithCode
+     * $message - message text / текст создаваемого сообщения
+     * $password_object_id - ID of created Password Object / ID созданного объекта пароля
+     * $phone - Phone number in international format / номер телефона в формате 375291234567
+     * $alphaname_id - Sender ID (optional) // ID альфа-имени, необязательный параметр
+     */
+    public function sendSmsMessageWithCode($message, $password_object_id, $phone, $alphaname_id = 0) {
+        $params['message'] = $message;
+        $params['password_object_id'] = (integer)$password_object_id;
+        $params['phone'] = $phone;
+        if (!empty($alphaname_id))
+            $params['alphaname_id'] = (integer)$alphaname_id;
+        return $this->sendRequest('sendSmsMessageWithCode', $params);
+    }
+
+    /**
+     * Метод-обёртка для команды getAlphaNames
+     */
+    public function getAlphaNames() {
+        return $this->sendRequest('getAlphanames');
+    }
+
+    /**
+     * Метод-обёртка для команды getAlphaNameId
+     */
+    public function getAlphaNameId($name) {
+        $params['name'] = $name;
+        return $this->sendRequest('getAlphanameId', $params);
+    }
+
+    /**
+     * Метод-обёртка для команды flashCall
+     * $phone - номер телефона в формате 375291234567
+     * $code - код подтверждения, если не указан сгенерируется автоматически
+     * $attempt - количество попыток для подтверждения кода, если не указано то 3
+     * $time_valid - время действия кода подтверждения в секундах, если не указано то 90
+     */
+    public function flashCall($phone, $code = '', $attempt = 0, $time_valid = 0) {
+        $params['phone'] = $phone;
+        if (!empty($code))
+            $params['code'] = $code;
+        if (!empty($attempt))
+            $params['attempt'] = (integer)$attempt;
+        if (!empty($time_valid))
+            $params['time_valid'] = (integer)$time_valid;
+        return $this->sendRequest('flashCall', $params, 'post');
+    }
+
+    /**
+     * Метод-обёртка для команды confirmFlashCall
+     * $phone - номер телефона в формате 375291234567
+     * $code - код подтверждения
+     * $fclid - значение fclid из метода flashCall
+     */
+    public function confirmFlashCall($phone, $code, $fclid) {
+        $params['phone'] = $phone;
+        $params['code'] = $code;
+        $params['fclid'] = $fclid;
+        return $this->sendRequest('confirmFlashCall', $params, 'post');
+    }
+
+    /**
+     * Метод-обёртка для команды getVibernames
+     */
+    public function getVibernames() {
+        return $this->sendRequest('getVibernames');
+    }
+
+    /**
+     * Метод-обёртка для команды createViberMessage
+     * $message - текст viber-сообщения
+     * $vibername_id - ID viber-имени
+     */
+    public function createViberMessage($message, $vibername_id) {
+        $params['message'] = $message;
+        $params['vibername_id'] = $vibername_id;
+        return $this->sendRequest('createViberMessage', $params, 'post');
+    }
+
+    /**
+     * Метод-обёртка для команды sendViberMessage
+     * $phone - номер телефона в формате 375291234567
+     * $viber_message_id - ID viber-сообщения из метода createViberMessage
+     */
+    public function sendViberMessage($phone, $viber_message_id) {
+        $params['phone'] = $phone;
+        $params['viber_message_id'] = $viber_message_id;
+        return $this->sendRequest('sendViberMessage', $params, 'post');
+    }
+
+    /**
+     * Метод-обёртка для команды sendQuickViberMessage - отправка viber-сообщения без предварительного его создания
+     * $phone - номер телефона в формате 375291234567
+     * $vibername_id - ID viber-имени
+     * $message - текст viber-сообщения
+     */
+    public function sendQuickViberMessage($phone, $vibername_id, $message) {
+        $params['phone'] = $phone;
+        $params['vibername_id'] = $vibername_id;
+        $params['message'] = $message;
+        return $this->sendRequest('sendQuickViberMessage', $params, 'post');
+    }
+
+
+        /**
+     * Метод-обёртка для команды sendQuickViberMessageWithImage - отправка viber-сообщения без предварительного его создания
+     * $phone - номер телефона в формате 375291234567
+     * $vibername_id - ID viber-имени
+     * $message - текст viber-сообщения
+     */
+    public function sendQuickViberMessageWithImage($phone, $vibername_id, $message, $type_message, $image, $button, $button_link) {
+        if ($image !== "") {
+            $mime = mime_content_type($image);
+            $img = new CURLFile($image, $mime, 'image.jpg');
+        }
+        if ($type_message === "TEXT") {
+            $data = array(
+                'phone' => $phone,
+                'vibername_id' => $vibername_id,
+                'message' => $message,
+                'type_message' => $type_message,
+              );
+        } elseif ($type_message === "IMAGE") {
+            $data = array(
+                'phone' => $phone,
+                'vibername_id' => $vibername_id,
+                'message' => $message,
+                'type_message' => $type_message,
+                'image' => $img ?? "",
+            );
+        } else {
+            $data = array(
+                'phone' => $phone,
+                'vibername_id' => $vibername_id,
+                'message' => $message,
+                'type_message' => $type_message,
+                'image' => $img,
+                'button' => $button,
+                'button_link' => $button_link,
+              );
+        }
+          $ch = curl_init($this->API_URL_v2 . "sendQuickViberMessage?token=" . $this->token);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+          curl_setopt($ch, CURLOPT_POST, true);
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+          curl_exec($ch);
+          curl_close($ch);
+    }
+
+    /**
+     * Метод-обёртка для команды sendViberMessageList
+     * $message - информация об viber-сообщении
+     * $name - название рассылки
+     * $vibername_id - ID viber-имени
+     * $list_id - ID списка рассылки
+     * $d_schedule - планируемое время отправки
+     */
+    public function sendViberMessageList($message, $name, $vibername_id, $list_id, $d_schedule) {
+        $params['name'] = $name;
+        $params['vibername_id'] = $vibername_id;
+        $params['list_id'] = $list_id;
+        $params['d_schedule'] = $d_schedule;
+        $params = array_merge($params, $message);
+        return $this->sendRequest('sendViberMessageList', $params, 'post');
+    }
+
+    /**
+     * Метод-обёртка для команды getViberMessageList
+     */
+    public function getViberMessageList() {
+        return $this->sendRequest('getViberMessageList');
+    }
+
+  }
---
+++ sms-php-main/examples.php
@@ -0,0 +1,174 @@
+<?php
+  include('SMS_BY.php');
+  require_once('Transliterate.php');
+  require_once('CountSmsParts.php');
+
+  // Your API-KEY or token which you can obtain here: https://app.sms.by/user-api/token
+  $token = '';  // Код токена вы можете получить здесь: https://app.sms.by/user-api/token
+  // Phone number where you will receive all test sms
+  $phone = '';  // Номер телефона для теста
+
+
+  $text = "Заглавная буква в начале текста"; // place here any sample text
+  $comment = "Пример работы транслитерации строки. \"$text\" "; // transliteration of russian text to english
+  $translit = Transliterate::getTransliteration($text);
+  _echo($comment, $translit);
+
+
+  $string = "Длина этого короткого текста на русском  примерно 70 символов или около того";
+  $oSize = new CountSmsParts($string);
+  $res = $oSize->checkTextLength($string);
+  _echo("Определяем размер сообщения \"$string\"", "Частей = ".$res['parts'].", длина=".$res['len']);
+
+  $sms = new SMS_BY($token);
+
+  /** Get Balance / Получение баланса  */
+  if (true) {
+    $res = $sms->getBalance();
+    _echo("Requesting balance", "Balance = ".$res->result[0]->balance." ".$res->currency);
+  }
+
+
+  if (false) {
+    $message = "Hello World! Powered by SMS.by";
+    $res = $sms->sendQuickSms($message, $phone);
+    _echo ("Sent sms using sendQuickSms method");
+  }
+
+  /** Send simple Sms message / Отправка простого сообщения */
+  if (false) {
+    $message = 'Hello from SMS.BY!';
+    _echo("Send sms-message '$message' на номер: $phone");
+    $res = $sms->createSMSMessage($message);
+    $message_id = $res->message_id;
+    $res = $sms->sendSms($message_id, $phone);
+    if ($res == false)
+      _echo ("Во время отправки сообщения произошла ошибка" );
+    else
+      _echo ("Сообщение успешно отправлено, его ID: {$res->sms_id}");
+  }
+
+  /** Отправка сообщения с паролем от альфа-имени */
+  if (false) {
+    _echo("Отправка сообщения с паролем от альфа-имени с ID = 0");
+    /** Если у вас пока нет собственного Альфа-имени, то вы можете тестировать от системного Альфа-имени с id=0 */
+    $alphaname_id = 0;
+    $res = $sms->createPasswordObject('both', 5);
+    $password_object_id = $res->result->password_object_id;
+
+    $res = $sms->sendSmsMessageWithCode('Ваш пароль: %CODE%', $password_object_id, $phone, $alphaname_id);
+    if ($res == false)
+      _echo("Во время отправки сообщения произошла ошибка");
+    else
+      _echo("Сообщение успешно отправлено, его ID: {$res[0]->sms_id}");
+  }
+
+  /**  Get a list of your messages / Получение списка своих сообщений */
+  if (false) {
+    $messages = $sms->getMessagesList();
+    echo "<pre>";
+    print_r($messages->result);
+    echo "</pre>";
+  }
+
+  /** Get a list of Sender IDs / Получение списка альфа-имён */
+  if (false) {
+    $alpha_names = $sms->getAlphaNames();
+    echo "<pre>";
+    print_r($alpha_names);
+    echo "</pre>";
+  }
+
+  /**  Получение ID Альфа имени */
+  if (false) {
+    $name = '0'; // Ваше альфа-имя
+    $alphaNameId = $sms->getAlphaNameId($name);
+    echo "<pre>";
+    print_r($alphaNameId);
+    echo "</pre>";
+  }
+
+  /** FlashCall */
+  if (false) {
+    $res = $sms->flashCall($phone);  // Создание FlashCall
+    if ($res->status=='success') {
+      $code = $res->data->code;
+      $fclid = $res->data->fclid;
+
+      $res = $sms->confirmFlashCall($phone, $code, $fclid);  // Проверка кода FlashCall
+      if ($res->status=='success')
+        _echo("Код подтверждён");
+      else
+        _echo("Во время проверки кода произошла ошибка");
+    }
+    else
+      _echo("Во время создания FlashCall произошла ошибка");
+  }
+
+  /**  Получение списка viber-имён */
+  if (false) {
+    $viver_names = $sms->getVibernames();
+    echo "<pre>";
+    print_r($viver_names);
+    echo "</pre>";
+  }
+
+  /** Отправка простого viber-сообщения */
+  if (false) {
+    $message = 'Привет от sms.by!';
+    $vibername_id = 0;  // ID вашего viber-имени
+    $res = $sms->sendQuickViberMessage($phone, $vibername_id, $message);
+    if (isset($res->status) and $res->status=='OK')
+      _echo("Отправка sms-сообщения '$message' на номер: $phone", "Сообщение успешно отправлено, его ID: {$res->result->viber_id}");
+    else
+      _echo("Отправка sms-сообщения '$message' на номер: $phone", "Во время отправки сообщения произошла ошибка");
+  }
+
+  /** Отправка простого viber-сообщения с картинкой */
+  if (false) {
+    $message = 'Привет от sms.by!';
+    $vibername_id = 0;  // ID вашего viber-имени
+    $type_message = 'TEXT'; // Message type
+    $image = 'picture.jpg'; // Image
+    // $button = ''; // Button
+    // $button_link = ''; // Link
+
+    $sms->sendQuickViberMessageWithImage($phone, $vibername_id, $message, $type_message, $image, $button, $button_link);
+  }
+
+  /**  Отправка viber-сообщения списку рассылки */
+  if (false) {
+    $message = [  // информация о передаваемом сообщении
+      'type_message' => 'BUTTON',  // тип сообщения
+      'message' => 'Привет от sms.by!',  // текст сообщения
+      'button' => 'Нажми здесь',  // текст кнопки
+      'button_link' => 'https://sms.by'  // ссылка кнопки
+    ];
+    $name = 'Тестовая рассылка';
+    $vibername_id = 0;  // ID вашего viber-имени
+    $list_id = 0;  // ID вашего списка рассылки
+    $d_schedule = '2022-03-01 12:00';
+    $res = $sms->sendViberMessageList($message, $name, $vibername_id, $list_id, $d_schedule);
+    if (isset($res->status) and $res->status=='OK')
+      _echo("Отправка viber-сообщения списку рассылки", "Рассылка успешно создана, её ID: {$res->result->viber_id}");
+    else
+      _echo("Отправка viber-сообщения списку рассылки", "Во время отправки сообщения произошла ошибка");
+  }
+
+  /**  Получение списка своих viber-сообщений */
+  if (false) {
+    $messages = $sms->getViberMessageList();
+    echo "<pre>";
+    print_r($messages);
+    echo "</pre>";
+  }
+
+
+  function _echo($comment, $result="") {
+    $web = $_SERVER['REQUEST_METHOD'] ?? false;
+    $d = (!$web) ? "\n" : "<br />";
+    echo "$d";
+    echo "<b>Действие:</b> $comment $d" ;
+    if(!empty($result))
+      echo "<b>Результат:</b> $result $d $d";
+  }
